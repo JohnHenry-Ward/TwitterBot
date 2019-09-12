e0f18cafc03074d82378152345d475d5e3b2f755
JohnHenry <johnhenry514@gmail.com>
2019-09-11 18:43:51 -0700

    authentication tokens are now hidden

--- bots/config.py
+++ bots/config.py
@@ -1,14 +1,15 @@
 import tweepy
 import logging
+from os import environ
 
 logger = logging.getLogger()
 
 # Strore authentication credentials
 def create_api():
-    consumer_key = "yr8JszgNjmhizjmq1jMXmcoS2"
-    consumer_secret = "x77un5VKszl2dYQeZkXZzju68346Iz3olgCAIEhbOb5aoQ4Wcv"
-    access_token = "1171920714867204096-IWvwfOljOyQyPQdacrwIvC4AuZtC2S"
-    access_token_secret = "gGKgVr7Rb3upOQ4yu65mxbDHuXFEiLD1erfqXuSPD3Fne"
+    consumer_key = environ["CONSUMER_KEY"]
+    consumer_secret = environ["CONSUMER_KEY_SECRET"]
+    access_token = environ["ACCESS_TOKEN"]
+    access_token_secret = environ["ACCESS_TOKEN_SECRET"]
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
