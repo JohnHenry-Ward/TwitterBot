a6263fe6bd3ee4f25b85e418406bd5dedd3bb35b
JohnHenry <johnhenry514@gmail.com>
2019-09-11 18:05:45 -0700

    created script that tweets once per day how many days til christmas

---
+++ bots/daysToChristmas.py
@@ -0,0 +1,26 @@
+import tweepy
+import logging
+from config import create_api
+import time
+import datetime
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def daysToChristmas():
+    logger.info("Calculating days til December 25")
+    today = datetime.date.today()
+    christmas = datetime.date(2019, 12, 25)
+    daysTil = christmas - today
+    return daysTil.days
+
+def main():
+    api = create_api()
+    while True:
+        daysTil = daysToChristmas()
+        logger.info("Tweeted: " + str(daysTil) + " days til Christmas.")
+        logger.info("Be back in 24 hours...")
+        time.sleep(86400)
+
+if __name__ == "__main__":
+    main()
